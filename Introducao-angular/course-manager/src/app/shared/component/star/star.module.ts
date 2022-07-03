08cffa91ea4cb9a520f147a060a6259b12acecd3
marioMechi <mariomvm@gmail.com>
2022-07-02 21:04:30 -0300

    Introdução Angular

---
+++ Introducao-angular/course-manager/src/app/shared/component/star/star.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+import { StarComponent } from "./star.component";
+
+@NgModule({
+    declarations:[
+        StarComponent
+    ],
+    imports:[
+        StarComponent
+    ]
+})
+
+export class starModule {
+
+}
